87198c0d2aeea53b02e4915fc2a5f5d53c86e338
kernbeisser <kernbeisser@tom-bonn.de>
2021-12-04 09:10:53 +0100

    inital commit

---
+++ greet.h
@@ -0,0 +1,8 @@
+# ifndef GREET_H
+# define GREET_H
+char* servus()
+{
+    return "Huhu, Tommi!";
+}
+
+# endif
---
+++ test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "greet.h"
+
+int main()
+{
+    printf("%s\n", servus());
+    return 0;
+}
